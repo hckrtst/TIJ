c10f57328ddcf423a1587841405fcf502291959c
bobcat <hckrtst@gmail.com>
2015-06-20 16:44:57 -0700

    more on sets

--- SetExample1/src/setexample1/SetExample1.java
+++ SetExample1/src/setexample1/SetExample1.java
@@ -25,6 +25,7 @@ package setexample1;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  *
@@ -36,6 +37,7 @@ public class SetExample1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("A set of strings");
         Set<String> agents = new HashSet<>();
         
         agents.add("James Bond");
@@ -51,5 +53,67 @@ public class SetExample1 {
         if (agents.isEmpty()) {
             System.out.println("Set is now empty");
         }        
+        
+
+        System.out.println("\nTry a generic set");
+        // Generic set
+        Set objs = new HashSet<>();
+        
+        objs.add("simple string");
+        objs.add(98753);
+        objs.add(2.34567);
+        
+        for (Object o: objs) {
+            System.out.println(o);
+        }
+        
+        
+        // Tree set
+        System.out.println("\nTry a tree set");
+        Set ts = new TreeSet<>();
+        ts.add(27865);
+        ts.add(123);
+        /*
+        Trying to add float will result in an exception since integers and 
+        floats are not comparable to each other
+        Exception in thread "main" java.lang.ClassCastException: java.lang.Integer cannot be cast to java.lang.Double
+	at java.lang.Double.compareTo(Double.java:49)
+	at java.util.TreeMap.put(TreeMap.java:568)
+	at java.util.TreeSet.add(TreeSet.java:255)
+	at setexample1.SetExample1.main(SetExample1.java:76)
+        */
+        // ts.add(3.14); 
+        ts.add(345);
+        ts.add(10);
+        ts.add(1);
+        
+        for (Object i : ts) {
+            System.out.println(i);
+        }
+        
+        
     }    
 }
+/*
+run:
+A set of strings
+Remington Steele
+James Bond
+Set is now empty
+
+Try a generic set
+98753
+2.34567
+simple string
+
+if we try to add a float to ts then we get
+
+Try a tree set
+Exception in thread "main" java.lang.ClassCastException: java.lang.Integer cannot be cast to java.lang.Double
+	at java.lang.Double.compareTo(Double.java:49)
+	at java.util.TreeMap.put(TreeMap.java:568)
+	at java.util.TreeSet.add(TreeSet.java:255)
+	at setexample1.SetExample1.main(SetExample1.java:76)
+
+
+*/
