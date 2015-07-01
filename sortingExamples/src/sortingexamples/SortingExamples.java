de51686031485bc224dd86e745690b3add179f6d
bobcat <hckrtst@gmail.com>
2015-07-01 08:14:12 -0700

    Some minor cleanups.
    Happy Canada Day!

--- sortingExamples/src/sortingexamples/SortingExamples.java
+++ sortingExamples/src/sortingexamples/SortingExamples.java
@@ -34,21 +34,14 @@ import java.util.logging.Logger;
  * @author Sanket K
  */
 public class SortingExamples {
-    List<Integer> data;
+    
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {    
         SortingExamples obj = new SortingExamples();
-        
-        System.out.println("Unsorted list = " + obj.getUnsorted());
-        
-        System.out.println("Sorted list = " + obj.insertionSort(obj.getUnsorted()));
-    }
-
-    public SortingExamples() {
-        this.data = new ArrayList<>();
+        List<Integer> data = new ArrayList<>();
         
         // TODO add random numbers in a loop
         data.add(100);
@@ -57,11 +50,16 @@ public class SortingExamples {
         data.add(3);
         data.add(3);
         data.add(3);
+        System.out.println("Unsorted list = " + data);
+        
+        if (obj.insertionSort(data)) {
+            System.out.println("Sorted list = " + data);
+        }
     }
     
-    public List<Integer> getUnsorted() {
-        return data;
-    }
+    public SortingExamples() {
+        
+    }    
     
     private void dumpState(int j, List<Integer> list) {
         for(int i =0; i < list.size(); i++ ) {
@@ -74,30 +72,44 @@ public class SortingExamples {
            System.out.print(", ");
         }
         try {
-            TimeUnit.SECONDS.sleep(7);
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException ex) {
             Logger.getLogger(SortingExamples.class.getName()).log(Level.SEVERE, null, ex);
         }
         System.out.println("");
     }
-    public List<Integer> insertionSort(List<Integer> unsorted) {
-        List<Integer> sorted = unsorted;
-        for (int i = 1; i < unsorted.size(); i++) {
+    
+    public boolean insertionSort(List<Integer> mylist) {
+        if (null == mylist) {
+            System.out.println("Cannot sort a null list");
+            return false;
+        }
+        
+        if (mylist.isEmpty()) {
+            System.out.println("Cannot sort an empty list");
+            return false;
+        }
+        
+        for (int i = 1; i < mylist.size(); i++) {
             int j = i;
-            int testvar = unsorted.get(i);
+            int testvar = mylist.get(i);
             System.out.print("Testing " + testvar + ": ");
-            dumpState(j, data);
+            dumpState(j, mylist);
             
-            while (j > 0 && unsorted.get(j-1) > testvar ) {
-                System.out.println(unsorted.get(j-1) + " > " + testvar);
-                unsorted.set(j, unsorted.get(j-1));
+            while (j > 0 && mylist.get(j-1) > testvar ) {
+                System.out.println(mylist.get(j-1) + " > " + testvar);
+                mylist.set(j, mylist.get(j-1));
                 //unsorted.set(j-1,0);
                 j = j-1;
                 
                 //dumpState(testvar, data);
             }
-            unsorted.set(j, testvar);
+            mylist.set(j, testvar);
         }
-        return sorted;
-    }    
+        return true;
+    }
+    
+    public boolean shellSort(List<Integer> unsorted) {
+        return false;
+    }
 }
