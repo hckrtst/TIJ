2c6e1412e768203400ba1415d199d1f6a171311e
bobcat <hckrtst@gmail.com>
2015-07-03 12:38:57 -0700

    more regex

--- regexTest/src/regextest/RegexTest.java
+++ regexTest/src/regextest/RegexTest.java
@@ -23,6 +23,10 @@
  */
 package regextest;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Sanket K
@@ -33,7 +37,100 @@ public class RegexTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String s = "The lazy dog\\nsleeps in the yard.";
         
+        System.out.println("\n\n ~~~ Splitting strings with regex ~~~");
+        System.out.println("\nUsing string: " + s);
+        System.out.println("\nSplit on \"\" ");
+        System.out.println(Arrays.toString(s.split("")));
+        
+        System.out.println("\nSplit on non-word ");
+        System.out.println(Arrays.toString(s.split("\\W")));
+        
+        String s2 = "Then came the downfall of the ninja";
+        System.out.println("\n\nUsing string: " + s2);
+        System.out.println("\nSplit on word n");
+        System.out.println(Arrays.toString(s2.split("(n)+")));
+        
+               
+        // Build a pattern and try to see if the pattern is matched in the 
+        // source string
+        // TODO - move to a static method
+        String ps = ".*down.*";
+        System.out.println("\nLooking for pattern: " + ps);
+        Pattern p = Pattern.compile(ps, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(s2);
+        System.out.println("Found = " + m.matches());
+        
+        String s3 = "Hits by kei$ha are on sale?! Well that's not surprising! $$$ ";
+        System.out.println("\nUsing string: " + s3);
+                
+        ps = ".*\\w+\\$\\w+.*";
+        System.out.println("\nLooking for pattern: " + ps);
+        p = Pattern.compile(ps, Pattern.CASE_INSENSITIVE);
+        m = p.matcher(s3);
+        System.out.println("Found = " + m.matches());
+        //System.out.println("Found between " + m.start() + " and " + m.end());
+        // Notice that the following will not run
+        while(m.find()) {
+            System.out.println("Groups = " + m.group());
+        }
+        
+        // We cannot use the matches() method to search for multiple occurrences of a regular expression. 
+        // For this, we need to use the find(), start() and end() methods. 
+        String s4 = "Tintin and snowy were skiing in the Chong chang region,\nwhen suddenly the gang of thugs came running";
+        System.out.println("\nUsing string: " + s4);
+        
+        ps = "\\s+[\\w]+g+.*";
+        System.out.println("\nLooking for pattern: " + ps);
+        p = Pattern.compile(ps, Pattern.CASE_INSENSITIVE);
+        m = p.matcher(s4);
+        System.out.println("Found = " + m.matches());
+        while(m.find()) {
+            System.out.println("Groups = " + m.group());
+        }
+        
+        ps = "\\s+[\\w]+g+\\S+";
+        System.out.println("\nLooking for pattern: " + ps);
+        p = Pattern.compile(ps, Pattern.CASE_INSENSITIVE);
+        m = p.matcher(s4);
+        System.out.println("Found = " + m.matches());
+        while(m.find()) {
+            System.out.println("Groups = " + m.group());
+        }
+        
+        ps = "\\s+[\\w]+g+\\s+";
+        System.out.println("\nLooking for pattern: " + ps);
+        p = Pattern.compile(ps, Pattern.CASE_INSENSITIVE);
+        m = p.matcher(s4);
+        System.out.println("Found = " + m.matches());
+        while(m.find()) {
+            System.out.println("Groups = " + m.group());
+        }
+        
+        
+        String s5 = "MyCompany Inc. Copyright 1998-2012";
+        ps = ".*copyright\\s+\\d";
+        System.out.println("Length = " + s5.length());
+        System.out.println("\nUsing string: " + s5);
+        if (s5.matches("2015")) {
+            System.out.println("Copyright is updated");
+        } else {
+            System.out.println("Copyright is not updated");
+            s5 = s5.replaceAll(ps, "2015");
+            System.out.println("Updated = " + s5);
+        }
+        
+        
+        System.out.println("\nLooking for pattern: " + ps);
+        p = Pattern.compile(ps, Pattern.CASE_INSENSITIVE);
+        m = p.matcher(s5);
+        //System.out.println("lookingAt = " + m.lookingAt());
+        while(m.find()) {
+            System.out.println("Groups = " + m.group() + " at " + m.start() + " ends at " + m.end());
+            s5 = s5.replaceFirst(ps, "2015");
+        }
+        System.out.println("String is now = " +s5);
         
         
     }
