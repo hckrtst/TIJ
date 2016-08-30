68bd9493d4338bc7794918fa753d409fc37c531e
Sanket K <hckrtst@gmail.com>
2016-08-29 18:57:59 -0700

    scrub

--- AnagramTester/src/anagramtester/AnagramTester.java
+++ AnagramTester/src/anagramtester/AnagramTester.java
@@ -1,7 +1,7 @@
 /*
  The MIT License (MIT)
 
-Copyright (c) 2015 Sanket Khidkikar
+Copyright (c) 2015 Sanket
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
